a6e4d9c2e7b5e1ba7911b17260817595fa3b465a
Herman <547621835@qq.com>
2020-11-09 14:57:38 +0800

    first commit hhhhhhhhh

--- src/club/banyuan/demo/Main.java
+++ src/club/banyuan/demo/Main.java
@@ -3,6 +3,6 @@ package club.banyuan.demo;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello World"); // 打印 Hello World// write your code here
     }
 }
